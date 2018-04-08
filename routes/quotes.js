cc11d410ccd3559c5b2b737ee0a5fa9ba2ba6125
Dvontre A Coleman <dvontrec@gmail.com>
2018-04-08 13:08:19 -0400

    Add json response

--- routes/quotes.js
+++ routes/quotes.js
@@ -6,17 +6,25 @@ var db = require("../models");
 router.post("/", function(req, res){
 	db.Quote.create(req.body, function(err, newQuote){
 		if(err){
-			res.send("broken here");
+			res.send(err);
 		}
 		else{
-			res.status(201).json(newToDo);
+			res.status(201).json(newQuote);
 			console.log("Quote Created");
 		}
 	})
 })
 //		READ
 router.get("/", function(req, res){
-	res.send({"test": "pass"});
+	//finds all quotes in the database
+	db.Quote.find({}, function(err, allQuotes){
+		if(err){res.send(err);}
+		else
+		{
+			//sends all events in the database as jason
+			res.json(allQuotes);
+		}
+	})
 })
 //		EDIT
 //		UPDATE
