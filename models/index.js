84f5aa1b7c17f79e20bced415eee0e543bcd2bee
Dvontre Coleman <dvontrec@gmail.com>
2018-04-09 12:55:07 -0400

    Add remote DB for testing

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
 //connect to mongoose
 var mongoose = require("mongoose");
+const url = process.env.DATABASEURL;
 
 //allow debugging
 mongoose.set("debug", true);
 
 //Create/Connect to database
-mongoose.connect("mongodb://localhost/sprinkle-of-jeezy-api");
+mongoose.connect(url);
 
 //allows use of promises
 mongoose.Promise = Promise;
