6907c4b5037be8af7bbaf777231d2775ad6c914c
Dvontre Coleman <dvontrec@gmail.com>
2018-08-05 10:13:27 -0400

    Update gitignore for local env testing

--- models/quote.js
+++ models/quote.js
@@ -1,30 +1,29 @@
 //************************
-//			Qoute Model
+//			Quote Model
 //************************
-var mongoose = require("mongoose");
-
+var mongoose = require('mongoose');
 
 //creates a quote Schema
 var quoteSchema = mongoose.Schema({
 	//the Quote will be a string
 	quote: {
 		type: String,
-		required: "Cannot leave Quote Blank"
+		required: 'Cannot leave Quote Blank'
 	},
 	//the Artists name will be saved as a string
 	artist: {
 		type: String,
-		required: "Cannot leave Artist Blank"
+		required: 'Cannot leave Artist Blank'
 	},
 	//the song will be saved as a string
 	song: {
 		type: String,
-		required: "Cannot leave Song Blank"
+		required: 'Cannot leave Song Blank'
 	} //may change to link.
-})
+});
 
 //turns the schema to a model
-var Quote = mongoose.model("Quote", quoteSchema);
+var Quote = mongoose.model('Quote', quoteSchema);
 
 //exports the Quote model
 module.exports = Quote;
