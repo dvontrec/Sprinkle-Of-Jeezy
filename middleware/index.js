583ea64be8db4f7acd00565cb5b550612a55fd07
Dvontre Coleman <dvontrec@gmail.com>
2018-08-26 16:32:50 -0400

    update middleware for spotify users

--- middleware/index.js
+++ middleware/index.js
@@ -5,52 +5,43 @@ var middlewareObj = {};
 //		MiddleWare
 //********************************
 //checks to see if the current session is logged in
-middlewareObj.isLoggedIn = function(req, res, next)
-{
+middlewareObj.isLoggedIn = function(req, res, next) {
 	//checks to see if the request is from an authenticated user
-	if(req.isAuthenticated())
-	{
+	if (req.isAuthenticated() && req.user.salt) {
 		//moves on to the next function.  usually the callback
-		return next(); 
+		return next();
 	}
 	//if the user is not authenticated we will continue.  No need for an else because we return if conditions are met
-	else
-	{
+	else {
 		req.flash('error', 'You need to be logged in to do that');
-		res.redirect("/login");
+		res.redirect('/login');
 	}
-}
+};
 
 //middleware used to see if the user logged in is an admin
-middlewareObj.isAdmin = function(req, res, next)
-{
+middlewareObj.isAdmin = function(req, res, next) {
 	//checks to see if the request is from an authenticated user
-	if(req.isAuthenticated())
-	{
+	if (req.isAuthenticated() && req.user.salt) {
 		//moves on to the next function.  usually the callback
-		return next(); 
+		return next();
 	}
 
 	//else only show the json of quotes
-	else
-	{
-		res.redirect("/api/quotes")
+	else {
+		res.redirect('/api/quotes');
 	}
-}
+};
 
 //function used to sanitize the request body
-middlewareObj.sanitizeBody = function(req, res, next)
-{
+middlewareObj.sanitizeBody = function(req, res, next) {
 	//loops through each key in the body
-	for(var key in req.body)
-	{
+	for (var key in req.body) {
 		//changes the key to match a sanitized version of itself
 		req.body[key] = req.sanitize(req.body[key]);
 	}
 	//calls the next function
 	next();
-}
-
+};
 
 //exports the middleware object
 module.exports = middlewareObj;
